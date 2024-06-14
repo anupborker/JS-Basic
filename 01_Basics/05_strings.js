5c6665533141d661e9cc709d249ea95345aaa349
anupborker <anupborker@gmail.com>
2024-06-14 09:02:44 +0530

    String Basics JS

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,15 +1,16 @@
-let repocount = 50;
 let name = "hitesh";
+let repocount = 50;
 
-console.log(`Hello my name is ${name} and my repo count is ${repocount}. `);
+
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}. `);
 
 const gameName = new String("AnupBorker");
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.toUpperCase());
-console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
 
 
-
+console.log(gameName.charAt(3))
