e8d195d1592fdfd5871c4c929ec895428c2ad8c9
anupborker <anupborker@gmail.com>
2024-06-14 10:26:41 +0530

    JS Strings Basics

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -9,15 +9,25 @@ const gameName = new String("AnupBorker");
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
-// console.log(gameName.toUpperCase());
+//console.log(gameName.toUpperCase());
 // console.log(gameName.length);
 
 
-console.log(gameName.charAt(3));
+// console.log(gameName.charAt(3));
 
-const newString = gameName.substring(0 , 4);
-console.log(newString);
+// const newString = gameName.substring(0 , 4);
+// console.log(newString);
 
 
-const newString1 = gameName.substring(5, -4);
-console.log(newString1);
+// const newString1 = gameName.substring(5, -4);
+// console.log(newString1);
+
+console.log("ANup");
+
+const url = "https://anup.com/Anup%20Borker"
+console.log(url.replace("%20" , "-"));
+console.log(url.includes('sundar'));
+
+
+
+
--- 01_Basics/String.js
+++
@@ -1,4 +0,0 @@
-const name = "Anup";
-const count = 50;
-
-console.log( name + count + " Value"  );
